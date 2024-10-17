791b6223ffe52352467dc1837aa6795d940cd30a
Matthew Meredith <matthewmeredith775@gmail.com>
2024-10-17 00:37:28 -0400

    clerk, shadcn

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,21 @@
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  test: publicProcedure.query(() => ({ greeting: "hello" })),
+  test2: protectedProcedure.query(({ ctx }) => ({
+    greeting: `hello ${ctx.auth.userId}`,
+  })),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
